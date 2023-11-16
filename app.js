64fd6ff860789cf22ea1d04a54d8dc2ee2d683d0
geno223 <eugenio.jimenez22397@gmail.com>
2023-11-15 22:11:15 -0500

    landscaper can use teeth to cut grass

---
+++ app.js
@@ -0,0 +1,6 @@
+//You are starting a landscaping business, but all you have are your teeth.
+//Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
+
+console.log("hello world")
+
+alert("landscaper: user can use teeth to cut grass")
