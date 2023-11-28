18617a7567f857d707e8225cfeb3b44797606775
geno223 <eugenio.jimenez22397@gmail.com>
2023-11-27 20:07:45 -0500

    landscaper solved

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
-//You are starting a landscaping business, but all you have are your teeth.
-//Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
+const tools = [
+    {name: "Teeth", generates: 1, price: 0},
+    {name: "Scissors", generates: 5, price: 5},
+    {name: "PushMower", generates: 50, price: 25},
+    {name: "BatMower", generates: 100, price: 250},
+    {name: "Team", generates: 250, price: 500},
+]
 
-console.log("hello world")
+const landScaper = {
+    money: 0,
+    tool: 0,
+    winGame: false,
+}
 
-alert("landscaper: user can use teeth to cut grass")
+function cutGrass(){
+    const tool = tools[landScaper.tool]
+    alert(`Landscaper cut grass using ${tool.name} 
+    and make ${tool.generates}dollars`)
+    landScaper.money += tool.generates
+}
 
-let money = 0
+function upgrade(){
+    
+    if(landScaper.tool + 1 <= tools.length){
+    const nextTool = tools[landScaper.tool + 1];
+    if(nextTool.price < landScaper.money){
+        landScaper.money -= nextTool.price
+        landScaper.tool += 1
+    }
+} else {
+    alert("no more tools to upgrade")
+    }
+}
 
-let cutGrass = prompt("we cut the grass")
-
-if(cutGrass === "yes"){
-    console.log(cutGrass)
-    money = money + 5
-    console.log(money)
+function winCondition(){
+if(landScaper.tool === tools.length - 1 && landScaper.money >= 1000){
+        alert("Landscaper win scenario")
+        landScaper.winGame = true
+    }
+}
+alert("Welcome to Landscaper")
+while(!landScaper.winGame){
+    const response = prompt(`You currently have ${landScaper.money} dollars, [c]ut grass or [u]pgrade tools`)
+    if(response === 'c'){
+        cutGrass()
+    }
+    if(response === 'u'){
+        upgrade()
+    }
+    winCondition()
 }
